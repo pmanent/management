730037463a6da39ec46972064492929d1eaaca6e
pmanent <peremanent@pmanent.local>
2018-04-15 00:30:24 +0200

    New stuff

--- src/main/java/io/spring/guides/gs_producing_web_service/Country.java
+++ src/main/java/io/spring/guides/gs_producing_web_service/Country.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2018.04.14 a las 05:13:58 PM CEST 
+// Generado el: 2018.04.15 a las 12:01:44 AM CEST 
 //
 
 
